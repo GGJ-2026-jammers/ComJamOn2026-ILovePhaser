f953265e412503924b68e5de886845d436a837bd
Alejandro Bueno <alejandrobuenocur@gmail.com>
2026-02-19 17:43:15 +0100

    mas cambios(La roca)

--- src/scenes/boot.js
+++ src/scenes/boot.js
@@ -13,9 +13,9 @@ export default class BootScene extends Phaser.Scene{
 
     //todo lo del preload quitarlo y dejar solo lo justo y necesario (carga de sprites, audios ,ect)
     preload(){
+        this.load.image('laRoca', 'assets/images/laFuckingRoca.jpg');
         
         //BARRA DE CARGA, comentar si no funciona
-
         //2 barras
         var progressBar = this.add.graphics();
         var progressBox = this.add.graphics();
@@ -42,48 +42,18 @@ export default class BootScene extends Phaser.Scene{
         });
 
 
-
-
         //cuando termina la carga, llamar a la siguiente escena y dormir esta
         this.load.on('complete', () => {
+            console.log("KLK")
             this.scene.run('StartMenu');
             this.scene.sleep('boot');
         });
-
-
-        //atlas
-        // this.load.atlas('ui', 'srcJuego/ui/AtlasTexturas.png', 'srcJuego/ui/AtlasUI.json');
-
-        //images
-        //this.load.image('heart', 'srcJuego/ui/Corazon.png');
-        
-        //sprite sheets
-        //this.load.spritesheet('player', 'srcJuego/sprites/Character/with_hands/SpriteSheets/walkSheet.png',
-        //    { frameWidth: 204, frameHeight: 204});
-      
-
-        
-            
-        //sonidos
-        //this.load.audio('golpe','srcJuego/audio/golpe_VSDC.wav');
-
-        //json
-        //this.load.json('data', 'srcJuego/scripts/JSON/data.json');
-
-        //TILEMAP, comentar si no hace falta
-
-        //carga del tilemap
-        //this.load.tilemapTiledJSON('tilemap','srcJuego/tiled/prueba2.json');
-
-        //carga del tileset
-        //this.load.image('patronesTilemap','srcJuego/tiled/arte/Dungeon_Tileset.png');
     }
 
 
     //create vacio
     create(){
-        this.createAnims();
-        this.scene.start('menu')// menu es la key de la pantalla con el titulo del juego
+
     }
 
     createAnims(){
