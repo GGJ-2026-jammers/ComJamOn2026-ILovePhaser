55864e8b24e0447ac91e5989fc4ebd1e079f83d4
DiegoJG <kokote888@users.noreply.github.com>
2026-02-20 22:15:25 +0100

    cosas

--- src/scenes/title.js
+++ src/scenes/title.js
@@ -86,29 +86,21 @@ export default class Title extends Phaser.Scene {
 
     mainLoop(){
         if (this.mode === 'normal'){
-            do
-            {
-                if (this.currentWordIndex < this.maxWords / 3){this.nextWordTime = 3000}
-                else if (this.currentWordIndex < (this.maxWords / 3) * 2) {this.nextWordTime = 1700}
-                else if (this.currentWordIndex < this.maxWords) {this.nextWordTime = 1200}
-                let skipWord = false
-                let newWord = this.time.addEvent({
-                    delay: this.nextWordTime,
-                    callback: () => {
-                        skipWord = true
-                    }
-                })
-                // while(skipWord){
-                    
-                // }
-                this.currentWordIndex++
-            }
-            while (this.currentWordIndex < this.maxWords)
+            if (this.currentWordIndex < this.maxWords / 3){this.nextWordTime = 3000}
+            else if (this.currentWordIndex < (this.maxWords / 3) * 2) {this.nextWordTime = 1700}
+            else if (this.currentWordIndex < this.maxWords) {this.nextWordTime = 1200}
         }
     }
 
     nextWord(){
         this.currentWordIndex++;
         this.currentWord =  this.words[this.currentWordIndex]; 
+    }
+
+    update(t, dt){
+        if (this.currentWordIndex < this.maxWords / 3){this.nextWordTime = 3000}
+        else if (this.currentWordIndex < (this.maxWords / 3) * 2) {this.nextWordTime = 1700}
+        else if (this.currentWordIndex < this.maxWords) {this.nextWordTime = 1200}
+        
     }
 }
